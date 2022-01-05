fe596bebb2d3f490dbe6bc4b20d81ad8425df112
Minaev Alexey <manai.mina@gmail.com>
2022-01-05 22:43:40 +0300

    added test for building with resources

--- Tests/CityTests.cs
+++ Tests/CityTests.cs
@@ -9,19 +9,50 @@ namespace Miracles.Tests
         public void CanBuildFreeCard()
         {
             var city = new City();
-            var builded = city.Build(new Card());
-            Assert.True(builded);
+            var freeCard = new Card();
+
+            Assert.True(city.CanBuild(freeCard));
         }
 
         [Fact]
-        public void CantBuildWithoutResource()
+        public void CanBuildWithResources()
+        {
+            var city = new City();
+
+            var resourceCard = new Card();
+            resourceCard.Effect.Resource.Wood = 2;
+            city.Build(resourceCard);
+
+            var targetCard = new Card();
+            targetCard.Cost.Resource.Wood = 1;
+        
+            Assert.True(city.CanBuild(targetCard));
+        }
+
+        [Fact]
+        public void CantBuildWithoutResources()
         {
             var city = new City();
         
             var card = new Card();
-            card.Cost.Resource = new Resource { Wood = 1 };
+            card.Cost.Resource.Wood = 1;
 
             Assert.False(city.Build(card));
+        }
+
+        [Fact]
+        public void TestName()
+        {
+            var city = new City();
+
+            var resourceCard = new Card();
+            resourceCard.Effect.Resource.Wood = 1;
+            city.Build(resourceCard);
+
+            var targetCard = new Card();
+            targetCard.Cost.Resource.Brick = 1;
+
+            Assert.False(city.CanBuild(targetCard));
         }
     }
 }
